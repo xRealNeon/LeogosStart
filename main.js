b69fc7af247272a6c9768791eab4d8445bec48ac
NeoCode <amecus79@gmail.com>
2019-12-01 18:45:42 +0100

    Add German Language

--- main.js
+++ main.js
@@ -5,29 +5,29 @@ var customparam = url.searchParams.get("cparam");
 var sform = document.getElementById("search");
 var sfield = document.getElementById("searchf");
 
-switch (engine) {
+switch (engine == null ? null : engine.toLowerCase()) {
     case "google":
-        console.log("Set search engine: google");
+        console.log("Suchmaschine gesetzt auf: Google");
         sfield.placeholder = "Google Suche";
         sform.action = "https://www.google.de/search";
         break;
     case "duckduckgo":
-        console.log("Set search engine: duckduckgo");
+        console.log("Suchmaschine gesetzt auf: Duckduckgo");
         sfield.placeholder = "Duckduckgo Suche";
         sform.action = "https://duckduckgo.com/";
         break;
     case "bing":
-        console.log("Set search engine: bing");
+        console.log("Suchmaschine gesetzt auf: Bing");
         sfield.placeholder = "Bing Suche";
         sform.action = "https://www.bing.com/search";
         break;
     case "ecosia":
-        console.log("Set search engine: ecosia");
+        console.log("Suchmaschine gesetzt auf: Ecosia");
         sfield.placeholder = "Ecosia Suche";
         sform.action = "https://www.ecosia.org/search";
         break;
     default:
-        console.log("Set search engine: google");
+        console.log("Suchmaschine gesetzt auf: Google");
         sfield.placeholder = "Google Suche";
         break;
 }
@@ -35,11 +35,17 @@ switch (engine) {
 if (customurl != null) {
     var surl = new URL(customurl);
     sfield.placeholder = surl.hostname + " Suche";
-    console.log("Set custom search url: " + customurl);
+    console.log("Benutzerdefinierte Such-URL festgelegt: " + customurl);
     sform.action = customurl;
 }
 
 if (customparam != null) {
-    console.log("Set custom search param: " + customparam);
+    console.log("Benutzerdefinierten Suchparameter festgelegt: " + customparam);
     sfield.name = customparam;
 }
+
+console.log("Gratulation, du hast die Developer Tools gefunden dann zeige ich dir wie du die Suchmaschine ändern kannst.");
+console.log("Mit dem Parameter \"engine\" kannst du zwichen Google, Duckduckgo, Bing und Ecosia wechseln.");
+console.log("Wenn du eine andere Suchmaschine benutzen willst, kannst du die url der Suchmaschine mit dem \"curl\" Parameter und den Such Parameter mit dem \"cparam\" Parameter setzen.");
+console.log("Beispiel: https://example.com/?engine=ecosia");
+console.log("Beispiel: https://example.com/?curl=https://www.ecosia.org/search&cparam=q");
